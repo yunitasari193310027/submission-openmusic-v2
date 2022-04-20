4395ad2d98d58355d48b4eecadc92d9a2584ac67
Yunita Sari <ynt.yunitasari@gmail.com>
2022-04-21 02:53:15 +0700

    add table playlist_songs

---
+++ migrations/1650484188330_create-table-playlist-songs.js
@@ -0,0 +1,24 @@
+/* eslint-disable camelcase */
+
+exports.shorthands = undefined;
+
+exports.up = (pgm) => {
+  pgm.createTable('playlist_songs', {
+    id: {
+      type: 'VARCHAR(50)',
+      primaryKey: true,
+    },
+    playlist_id: {
+      type: 'VARCHAR(50)',
+      notNull: true,
+    },
+    song_id: {
+      type: 'VARCHAR(50)',
+      notNull: true,
+    },
+  });
+};
+
+exports.down = (pgm) => {
+  pgm.dropTable('playlist_songs');
+};
